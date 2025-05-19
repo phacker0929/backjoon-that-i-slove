f90a4916512f5feff097aed70eada5ef6ad20853
phacker0929 <90496760+phacker0929@users.noreply.github.com>
2025-05-19 22:32:07 +0900

    2025 5 19 월 백준1009번 정답추가본
    2025 5 19 월 백준1009번 정답추가본

--- 1009.py
+++ 1009.py
@@ -19,4 +19,33 @@ for i in range(len(arr)):
     elif a==2 or a==3 or a==7 or a==8:
         print((a**b)%10)
 
+'''
+import sys
+input = sys.stdin.readline
 
+# 각 밑(a%10)에 대응하는 지수의 주기(cycle) 리스트
+cycle = [1, 1, 4, 4, 2, 1, 1, 4, 4, 2]
+
+T = int(input())
+for _ in range(T):
+    a, b = map(int, input().split())
+    # 밑의 마지막 자리만 보면 됨
+    a %= 10
+    
+    # 해당 밑의 주기를 cycle[a]에서 가져와서 b를 주기 길이로 나눈 나머지를 구함
+    b %= cycle[a]
+    # 나머지가 0이면 주기의 맨 마지막이므로 cycle[a] 값으로 대체
+    if b == 0:
+        b = cycle[a]
+    
+    # a의 b제곱의 마지막 자리
+    result = pow(a, b, 10)
+    # 만약 결과가 0이면 문제에서 요구하는 출력은 10
+    if result == 0:
+        result = 10
+    
+    print(result)
+
+
+
+'''
